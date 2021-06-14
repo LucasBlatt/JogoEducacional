e90219b40d28eab7cd1b847f6a5e386b34931794
Lucas Blatt <blatt.lucas.2003@gmail.com>
2021-06-14 16:25:26 -0300

    Foram corrigidas algumas coisas na movimentação do personagem, também foi adicionado um ícone e um nome para o jogo

--- JogoEducacional.py
+++ JogoEducacional.py
@@ -1,6 +1,11 @@
 import pygame
+import random
 
 pygame.init()
+
+icone = pygame.image.load("assets/icone.png")
+pygame.display.set_caption("Junk Food Runner")
+pygame.display.set_icon(icone)
 
 largura = 800
 altura = 600
@@ -9,14 +14,23 @@ fps = pygame.time.Clock()
 
 fundo = pygame.image.load("assets/fundo.png")
 personagem = pygame.image.load("assets/personagem.png")
+porcarias = [pygame.image.load("assets/food1.png"), pygame.image.load("assets/food2.png"),
+    pygame.image.load("assets/food3.png"), pygame.image.load("assets/food4.png"),
+        pygame.image.load("assets/food5.png")]
+porcariasRandom = random.choice(porcarias)
 
 preto = (0, 0, 0)
 branco = (255, 255, 255)
 
 personagemPosicaoX = 380
 personagemPosicaoY = 405
-
 movimentoX = 0
+porcariasPosicaoX = 380
+porcariasPosicaoY = -220
+porcariasLargura = 50
+porcariasAltura = 22
+porcariasVelocidade = 5
+
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -36,9 +50,16 @@ while True:
     personagemPosicaoX = personagemPosicaoX + movimentoX
     if personagemPosicaoX < 0:
         personagemPosicaoX = 0
-    elif personagemPosicaoX > 800:
-        personagemPosicaoX = 800    
+    elif personagemPosicaoX > 760:
+        personagemPosicaoX = 760    
     display.blit(personagem, (personagemPosicaoX, personagemPosicaoY))
+
+    display.blit(porcariasRandom, (porcariasPosicaoX, porcariasPosicaoY))
+    porcariasPosicaoY = porcariasPosicaoY + porcariasVelocidade
+    if porcariasPosicaoY > altura:
+        porcariasPOsicaoY = -220
+        porcariasVelocidade += 1
+        porcariasPosicaoX = random.randrange(0,largura - 50)
 
     pygame.display.update()
     fps.tick(60)
