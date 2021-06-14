dac1bebdd5e24822788e7828a61215cb61e01476
Lucas Blatt <blatt.lucas.2003@gmail.com>
2021-06-14 15:25:19 -0300

    Foi adicionado o fundo e o personagem

--- JogoEducacional.py
+++ JogoEducacional.py
@@ -7,10 +7,19 @@ altura = 600
 display = pygame.display.set_mode((largura, altura))
 fps = pygame.time.Clock()
 
+fundo = pygame.image.load("assets/fundo.png")
+personagem = pygame.image.load("assets/personagem.png")
+
+preto = (0, 0, 0)
+branco = (255, 255, 255)
+
 while True:
-    for evento in pygame.event():
-        pygame.quit()
-        quit()
+    #for evento in pygame.event.get():
+        #pygame.quit()
+        #quit()
     
     pygame.display.update()
     fps.tick(60)
+    display.fill(branco)
+    display.blit(fundo, (0,0))
+    display.blit(personagem, (380, 400))
