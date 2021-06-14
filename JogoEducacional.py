7b22751edfc6b939526964b17ac307263862b79a
Lucas Blatt <blatt.lucas.2003@gmail.com>
2021-06-14 15:59:58 -0300

    Foi adicionado a movimentação do personagem e as 'porcarias'

--- JogoEducacional.py
+++ JogoEducacional.py
@@ -13,13 +13,32 @@ personagem = pygame.image.load("assets/personagem.png")
 preto = (0, 0, 0)
 branco = (255, 255, 255)
 
+personagemPosicaoX = 380
+personagemPosicaoY = 405
+
+movimentoX = 0
 while True:
-    #for evento in pygame.event.get():
-        #pygame.quit()
-        #quit()
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT:
+                movimentoX = -5
+            elif evento.key == pygame.K_RIGHT:
+                movimentoX = 5
+        if evento.type == pygame.KEYUP:
+            movimento = 0                
     
-    pygame.display.update()
-    fps.tick(60)
     display.fill(branco)
     display.blit(fundo, (0,0))
-    display.blit(personagem, (380, 400))
+    
+    personagemPosicaoX = personagemPosicaoX + movimentoX
+    if personagemPosicaoX < 0:
+        personagemPosicaoX = 0
+    elif personagemPosicaoX > 800:
+        personagemPosicaoX = 800    
+    display.blit(personagem, (personagemPosicaoX, personagemPosicaoY))
+
+    pygame.display.update()
+    fps.tick(60)
